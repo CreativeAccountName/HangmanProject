37df06539d7a62cf8c0b5cf0fd23ac3be5d08b63
Brendan Kear <privbrek@gmail.com>
2024-03-25 20:21:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import wordBank from "./word-bank.js";
+import prompt from "readline-sync";
+const answer = prompt.question("Does this work? ");
+console.log(answer);
+
+
+// Function to create random number
+const fetchRandom = () => {};
