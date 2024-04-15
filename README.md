2b5abaaa44a61797d923f24397ea3c55d1756f15
Brendan Kear <privbrek@gmail.com>
2024-04-15 13:19:59 -0400

    Touched up mechanics and finished readMe

--- README.md
+++ README.md
@@ -1,9 +1,55 @@
 # Hangman
 
-Update me with a description of:
+Hangman is a game developed for the Spring 2024 Javascript Fundamentals course from CanCode.
 
-- What is this project?
-- How can I install the game?
-- How do I play the game?
+# Installation
 
-You can see the [Markdown cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) on how to format your README.md file.
+1. Download the repository or create a fork of your own and then clone the fork.
+2. Open in or navigate to the repository's root folder in your preferred terminal interface.
+3. In your terminal, enter the following command to add node.js functionality your local copy:
+
+```
+npm init -y
+```
+
+4. Run the following to install the readline-sync module:
+
+```
+npm install --save readline-sync
+```
+
+## How to Run:
+
+1. Access your local copy's root folder in your terminal.
+2. Still in your terminal, type the following:
+
+```
+node .
+```
+
+3. The program will then display a splash screen and prompt the user for confirmation before starting the first round. There are two options:
+   - To start the game, type "y" and hit enter.
+   - To close the game instead, type "n" and hit enter.
+
+## How to Play:
+
+- Each round, the player will be provided a random, hidden word for them to reveal. This word is represented by a series of underscores positioned under the gallows.
+- The player is prompted to guess a letter of the alphabet.
+- Matching letters in the secret word will replace their corresponding underscores.
+- But watch out! Each incorrect guess will put the condemned one step closer to death.
+  - The player can incorrectly guess a total of six times without losing the round.
+
+### Winning Condition:
+
+- The player must guess every letter that's present in the secret word. Once there are no underscores remaining, the player wins, and the gallows are updated to display a congratulatory message.
+
+### Losing Condition:
+
+- The player will lose once they have guessed a seventh letter incorrectly, after which the gallows are updated to display a failure message.
+
+## Between Rounds:
+
+Whether the player has won or lost the last round, they will be notified how many wins and losses they've accrued, and they'll be asked to start a new round:
+
+- To start a new round, type "y" and hit enter. This will reset the rounds' values back to their initial states and produce an entirely different random word. Then a new round will begin.
+- To quit, type "n" and hit enter. This will display a final score page indicating total number of rounds, wins, losses, and the player's overall win percentage. To exit, just hit any key.
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import wordBank from "./word-bank.js";
 import prompt from "readline-sync";
-prompt.setDefaultOptions({ caseSensitive: false });
+//prompt.setDefaultOptions({ caseSensitive: false });
 
-let playerStat = { win: 0, loss: 0, rounds: 0 }; // creates a global object that keeps track of wins and losses.
-let gameState = { isPlaying: true, isGuessing: false }; //could roll gameStats into this directly, honestly
-// let gallows = []; // Array that will eventually hold the current board state.
+let playerStat = { win: 0, loss: 0, rounds: 0 }; // Creates a global object that keeps track of wins and losses.
+let gameState = { isPlaying: true, isGuessing: false }; // Creates a global object that keeps track of the player's wins and losses.
 
-//Initializes and stores the initial board state.
+// Initializes and stores the initial board state.
 const boardSetup = () => {
-	//Default board state. Initialized in this way so gallows can be iterated upon without losing access to this state or rebuilding the matrix elsewhere.
+	// Default board state. Initialized in this way so gallows can be iterated upon without losing access to this state or rebuilding the matrix elsewhere.
 	const matrix = [
 		[" ", " ", " ", " ", " ", " ", " ", " ", "#", "=", "=", "=", "=", "|", " ", " ", " ", " ", " "],
 		[" ", " ", " ", " ", " ", " ", " ", " ", "|", " ", "\\", " ", " ", "|", " ", " ", " ", " ", " "],
@@ -49,15 +48,13 @@ const setup = (bank, fetch) => {
 		hasLost: false, // As hasWon, but for when the player loses the round.
 	};
 
-	//Creates a copy of the secret word, splits it into an array of strings, and creates an array with placeholder underscores to display partially filled in text as players guess letters correctly.
+	// Creates a copy of the secret word, splits it into an array of strings, and creates an array with placeholder underscores to display partially filled in text as players guess letters correctly.
 	gData.finalAns = structuredClone(gData.secret).split("");
 
 	// Now that this array is the same length as the secret word, underscores replace its values so it can be drawn to the board after each guess is checked.
 	gData.finalAns.forEach((value, index) => {
 		if (value !== "_") gData.finalAns[index] = "_";
 	});
-
-	//use the function that takes a string and creates a new array out of the string variable;
 	return gData;
 };
 
@@ -66,7 +63,7 @@ let gameData = setup(wordBank, fetchRandom);
 
 //Parses player input.
 const parseAns = (gData, matrix, render) => {
-	//Creates a variable to store an upper-case version of the player's answer. Then if it perfectly matches the secret word, the game ends.
+	//Creates a variable to store an upper-case version of the player's answer. Then if it matches the secret word, the game ends with the player win condition triggered. Currently, the only input accepted outside of letters of the alphabet is the secret word because of how the limit option functions in readline-sync.
 	let ansComp = gData.playerAns.toLocaleUpperCase();
 	let isWordMatch = gData.secret === ansComp;
 
@@ -75,26 +72,27 @@ const parseAns = (gData, matrix, render) => {
 		return render(gData, matrix);
 	}
 
-	// If it's a new guess, it's added to gData.oldAns. Then a copy of the secret word is turned into an array searched for a letter matching the player's answer.
+	// If it's a new guess, then a copy of the secret word is turned into an array searched for a letter matching the player's answer.
 	else if (gData.isNew) {
-		// If player guesses correct word as a string, game ends immediately.
+		// If player guesses the word as a string, the game ends immediately.
 		if (isWordMatch) {
 			gData.hasWon = true;
 			return render(gData, matrix);
 		}
 
+		//Otherwise, the guess is added to gData.oldAns, a new array is built based on the secret word, and then the new array is searched for any matches to the player's guess.
 		const secretArray = structuredClone(gData.secret.split(""));
 		gData.isCorrect = secretArray.includes(ansComp);
-		// If there is no match, then the map case changes and the number of incorrect guesses left reduces.
+		// If there is no match, then the map case changes, the remaining number of incorrect guesses reduces, and the player is informed that their guess was incorrect.
 		if (!gData.isCorrect) {
-			console.log(`Sorry, but "${gData.playerAns}" isn't part the word!`);
+			console.log(`Sorry! The word doesn't contain "${gData.playerAns}".`);
 			gData.ansInc += 1;
 		}
 
-		// If there is a match, then for each index value of the secret word that matches in the final answer that is still "_", gData.finalAns[index] is overwritten to reflect the new information.
+		// If there is a match, then for each index value of the secret word that matches the final answer that is still "_", gData.finalAns[index] is overwritten to reflect the new information.
 		else if (gData.isCorrect) {
 			let i = 0;
-			console.log(`Yes! "${gData.playerAns}" is part the word!`);
+			console.log(`Yes! The word contains "${gData.playerAns}"!`);
 			for (let index of secretArray) {
 				if (index.includes(ansComp) && gData.finalAns[i] === "_") {
 					gData.finalAns[i] = gData.playerAns;
@@ -103,7 +101,8 @@ const parseAns = (gData, matrix, render) => {
 			}
 		}
 		let finalComp = gData.finalAns.join("").toLocaleUpperCase();
-		//This checks whether each letter in the secret word has been guessed correctly. If so, game
+
+		//This checks whether each letter in the secret word has been guessed correctly. If so, game ends immediately with the player win condition triggered.
 		if (finalComp === gData.secret) {
 			gData.hasWon = true;
 		}
@@ -113,7 +112,7 @@ const parseAns = (gData, matrix, render) => {
 	}
 };
 
-// Takes the original graphics and gameData, then makes changes before drawing a deep copy version based on
+// Takes the original graphics matrix and gameData, then makes changes before joining and drawing a deep copy version based on that initial matrix layout.
 const drawGame = (gData, matrix) => {
 	// Changes the appearance of the gallows based on the number of incorrect guesses made by the player.
 	switch (gData.ansInc) {
@@ -136,13 +135,22 @@ const drawGame = (gData, matrix) => {
 			break;
 		case 6:
 			matrix[3][7] = "\\";
+			break;
+		case 7:
+			matrix[3][7] = " ";
+			matrix[4][7] = "/";
+			matrix[3][9] = " ";
+			matrix[4][9] = "\\";
+			matrix[5][7] = " ";
+			matrix[5][8] = "‖";
+			matrix[5][9] = " ";
 			gData.hasLost = true;
 			break;
 		default:
 			console.log(`\n\n... I've no idea how you broke my render iteration code, but I haven't crashed yet, \nso let's pretend nothing ever happened, okay?\n\n`);
 	}
 
-	// If the parser function flagged that the player has won or lost the game, one of these will run to display the end state and end the isGuessing loop.
+	// If the parser function flagged that the player has won or lost the game, one of these will run to display the end state and end the isGuessing loop. In each case, two nested arrays in the matrix are replaced.
 	if (gData.hasWon) {
 		playerStat.win += 1;
 		gameState.isGuessing = false;
@@ -152,7 +160,6 @@ const drawGame = (gData, matrix) => {
 			[" ", " ", " ", "|", " ", "G", "A", "M", "E", " ", "O", "V", "E", "R", " ", "|", " ", " ", " "],
 			[" ", " ", " ", "|", " ", "Y", "O", "U", " ", "W", "I", "N", "!", " ", " ", "|", " ", " ", " "]
 		);
-		// matrix.splice(10, 1, [" ", " ", " ", "|", " ", " ", "Y", "O", "U", " ", "W", "I", "N", " ", " ", "|", " ", " ", " "]);
 	} else if (gData.hasLost) {
 		playerStat.loss += 1;
 		gameState.isGuessing = false;
@@ -162,19 +169,24 @@ const drawGame = (gData, matrix) => {
 			[" ", " ", " ", "|", " ", "G", "A", "M", "E", " ", "O", "V", "E", "R", " ", "|", " ", " ", " "],
 			[" ", " ", " ", "|", " ", "Y", "O", "U", " ", "L", "O", "S", "E", "!", " ", "|", " ", " ", " "]
 		);
-		// matrix.splice(10, 1, [" ", " ", " ", "|", " ", " ", "Y", "O", "O", " ", "L", "O", "S", "E", " ", "|", " ", " ", " "]);
 	}
 
 	let matMap = matrix.map((index) => index.join("")).join("\n"); // Turns a copy of the matrix into a string that draws each former nested array on a new line.
 	console.log(`\n${matMap}\n    ${gData.finalAns.join(" ")}\n\n`); // Draws the gallows and displays the current final answer status.
 	console.log(`Previous Guesses: ${gData.oldAns.join(" ")}`);
-	// If the round is over, this isn't necessary.
-	let tempTries = 6 - gData.ansInc;
+
+	// If the player guessed incorrectly for the last time, then gameState.isGuessing will have been set to false before it was passed here by the parser function. If that happens, then tempTries will never initialize, and the player will not be informed how many more times they can incorrectly guess before they lose.
 	if (gameState.isGuessing) {
-		if (tempTries !== 1) {
+		let tempTries = 6 - gData.ansInc; // Used to determine whether to use singular or plural case in the remaining tries message.
+
+		if (tempTries === 6) {
+			console.log(`You can guess incorrectly a total of 6 times.`);
+		} else if (6 > tempTries && tempTries > 1) {
 			console.log(`You can guess incorrectly ${tempTries} more times.`);
-		} else {
+		} else if (tempTries === 1) {
 			console.log(`You can guess incorrectly ${tempTries} more time.`);
+		} else {
+			console.log(`Careful! You have ${tempTries} incorrect guesses left!`);
 		}
 	}
 
@@ -187,15 +199,15 @@ const drawGame = (gData, matrix) => {
 	}
 
 	return gData;
-}; // Game loop doesn't end even if gameState.isGuessing = false. This is likely because the finalAns filling code- or much of the parser- is setting flags correctly.
+};
 
 // Asks if the user wants to start the game. If yes, then the game will run. If not, the game will run the shutdown script.
 console.log(
-	`\n∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎\n\n Welcome to the Hangman Guessing Game!\n---------------------------------------\n\nPress CTRL+C to stop at any time.\n∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎\n\n`
+	`\n∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎\n\n\n\n\n\n\n Welcome to the Hangman Guessing Game!\n---------------------------------------\n\nPress CTRL+C to stop at any time.\n\n\n\n\n\n∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎\n\n`
 );
 
 // Asks the user if they want to play the game. If so, the gameState.isPlaying while loop runs until the user decides to stop
-let startGame = prompt.question(`Would you like to start the game? Y/N:\n`, { limit: ["y", "n"], limitMessage: `I asked for "Y" or "N":\n`, trueValue: ["y"], falseValue: ["n"] });
+let startGame = prompt.question(`Would you like to start the game? Y/N: `, { limit: ["y", "n"], limitMessage: `I asked for "Y" or "N": `, trueValue: ["y"], falseValue: ["n"] });
 if (startGame === true) {
 	gameState.isGuessing = true;
 } else if (startGame === false) {
@@ -213,12 +225,15 @@ while (gameState.isPlaying) {
 	while (gameState.isGuessing) {
 		gameData.playerAns = prompt.question(`Guess a letter: `, {
 			limit: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", `${gameData.secret}`],
-			limitMessage: "\nOops! That's not a valid guess.",
+			limitMessage: "\nSorry, I didn't quite catch that. Could you try something else? ",
 		});
-		console.log(`\n`);
+		console.log(`\n`); // Here for formatting purposes.
+
+		// Creates uppercase and lowercase variable strings based on the players' answer, then checks gameData.oldAns for both upper and lowercase versions already included in the array.
 		let hasAnsUp = gameData.oldAns.includes(gameData.playerAns.toLocaleUpperCase());
 		let hasAnsDown = gameData.oldAns.includes(gameData.playerAns.toLocaleLowerCase());
 
+		// If there is a match from either string variable, gameData.isNew is set to true, and duplicate answers will not count against the player nor will they be considered when it's time to draw the game again.
 		if (hasAnsUp || hasAnsDown) {
 			gameData.isNew = false;
 		} else {
@@ -228,10 +243,10 @@ while (gameState.isPlaying) {
 		parseAns(gameData, gallows, drawGame);
 	}
 
-	// If the round is over, this will ask the player if they want to play again. If they do,
+	// If the round is over, this will ask the player if they want to play again and display the number of rounds they've won and lost. If they do, gameData will be reset with a new secret word, the gallows matrix will be reset, and the gameState.isGuessing boolean will be set to true. Otherwise, the player will be shown a final stats page with an exit message, which they can linger on until they hit a key to exit the program.
 	if (!gameState.isGuessing) {
 		console.log(`Wins: ${playerStat.win} | Losses: ${playerStat.loss} \n`);
-		let newGame = prompt.question(`Would you like to play again? Y/N:\n`, {
+		let newGame = prompt.question(`Would you like to play again? Y/N: `, {
 			limit: ["Y", "N"],
 			limitMessage: `I asked for "Y" or "N":\n`,
 			trueValue: ["y"],
@@ -247,8 +262,9 @@ while (gameState.isPlaying) {
 				gameState.isPlaying = false;
 				let winPercent = (playerStat.win / playerStat.rounds) * 100;
 				let percentRound = Number(winPercent.toFixed(4));
-				prompt.question(
-					`\n################\n\nFinal Stats: \n  Total Rounds: ${playerStat.rounds}\n  Wins: ${playerStat.win}\n  Losses: ${playerStat.loss}\n  Win %: ${percentRound}%\n\n################\nThanks for playing!\n\nPress the "any" key to exit: `
+				prompt.keyInPause(
+					`\n################\n\nFinal Stats: \n  Total Rounds: ${playerStat.rounds}\n  Wins: ${playerStat.win}\n  Losses: ${playerStat.loss}\n  Win %: ${percentRound}%\n\n################\nThanks for playing!\n\nPress the "any" key to exit: `,
+					{ hideEchoBack: true, mask: " ", defaultInput: " " }
 				);
 				break;
 		}
